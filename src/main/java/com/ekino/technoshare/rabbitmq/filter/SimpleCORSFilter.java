6298f4d0d56559432afa45d16cd599945e5cf116
lepnio <lepnio@gmail.com>
2014-06-14 09:08:58 +0200

    correcting CORS filter

--- src/main/java/com/ekino/technoshare/rabbitmq/filter/SimpleCORSFilter.java
+++ src/main/java/com/ekino/technoshare/rabbitmq/filter/SimpleCORSFilter.java
@@ -16,9 +16,9 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:9000");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         chain.doFilter(req, res);
     }
 
